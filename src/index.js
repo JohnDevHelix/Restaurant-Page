857de8656dc086f69f81d6118f800a3c4f4d3a11
JohnDevHelixZeph <johndevproject@gmail.com>
2025-01-10 17:49:29 +0800

    Added nav bar divs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
+import { HomePage } from "./home";
 import { Menu } from "./menu";
+import { ContactUs } from "./contact";
 import "./style.css";
 
+let currentScreen = HomePage();
+const content = document.querySelector("#content");
+const createDiv = document.createElement("div");
+
+// nav bar
+const header = document.querySelector("header");
+const navs = document.querySelectorAll("nav");
+const headerDiv = createDiv.cloneNode(true);
+headerDiv.classList.add("header-div");
+const navsDiv = createDiv.cloneNode(true);
+navsDiv.classList.add("nav-div");
+header.appendChild(headerDiv);
+headerDiv.appendChild(navsDiv);
+navs.forEach((nav) => {
+  navsDiv.appendChild(nav);
+});
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.textContent === "Menu") {
+      content.innerHTML = "";
+      currentScreen = Menu();
+    } else if (button.textContent === "Contact Us") {
+      content.innerHTML = "";
+      currentScreen = ContactUs();
+    } else {
+      content.innerHTML = "";
+      currentScreen = HomePage();
+    }
+  });
+});
