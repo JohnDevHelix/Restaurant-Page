3f30df14e8288095d5172c9f30bff61eb6c1ec9d
JohnDevHelixZeph <johndevproject@gmail.com>
2025-01-13 13:12:34 +0800

    minor changes

--- src/contact.js
+++ src/contact.js
@@ -45,7 +45,7 @@ export function ContactUs() {
     '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><g data-name="27-Email-Message"><path d="M31.59 11.19 27 7.83V5a1 1 0 0 0-1-1h-4.22L16.59.19a1 1 0 0 0-1.18 0L10.22 4H6a1 1 0 0 0-1 1v2.83L.41 11.19A1 1 0 0 0 0 12v17a3 3 0 0 0 3 3h26a3 3 0 0 0 3-3V12a1 1 0 0 0-.41-.81zM27 10.31l2.2 1.61-2.2 1.31zM16 2.24 18.4 4h-4.8zM7 6h18v8.43l-9 5.4-9-5.4zm-2 4.31v2.93l-2.2-1.32zM2 28.59V13.77l9.26 5.56zM3.41 30 13 20.38l2.46 1.47a1 1 0 0 0 1 0L19 20.38 28.59 30zM30 28.59l-9.26-9.26L30 13.77z"/><path d="M10 9h12v2H10zM12 13h8v2h-8z"/></g></svg>';
   const h3Email = h3.cloneNode(true);
   divEmail.appendChild(h3Email);
-  h3Email.textContent = "kuyalouie.carenderia@gmail.com";
+  h3Email.textContent = "kuyalouie.carinderia@gmail.com";
 
   const divFB = createDiv.cloneNode(true);
   divFB.classList.add("contact");
@@ -75,8 +75,7 @@ export function ContactUs() {
     '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><defs><style>.cls-1{fill:#231f20}</style></defs><g id="location"><path class="cls-1" d="M22.37 21.09a1 1 0 0 0-1.3.56 1 1 0 0 0 .56 1.3C23.09 23.53 24 24.32 24 25c0 1.22-3.12 3-8 3s-8-1.78-8-3c0-.68.91-1.47 2.37-2.05a1 1 0 0 0 .56-1.3 1 1 0 0 0-1.3-.56C7.29 22 6 23.41 6 25c0 2.85 4.3 5 10 5s10-2.15 10-5c0-1.59-1.29-3-3.63-3.91z"/><path class="cls-1" d="M16 25a1 1 0 0 0 .68-.27C24 17.82 25.47 9.4 22.59 5.26a7.5 7.5 0 0 0-2.74-2.33 8.43 8.43 0 0 0-7.7 0 7.5 7.5 0 0 0-2.74 2.33C6.53 9.4 8 17.82 15.32 24.73A1 1 0 0 0 16 25zM11.05 6.4a5.5 5.5 0 0 1 2-1.69 6.4 6.4 0 0 1 5.86 0A5.5 5.5 0 0 1 21 6.4c2.2 3.25.9 10.25-5 16.2-5.9-5.95-7.2-12.95-4.95-16.2z"/><path class="cls-1" d="M19 9a3 3 0 1 0-3 3 3 3 0 0 0 3-3zm-4 0a1 1 0 1 1 1 1 1 1 0 0 1-1-1z"/></g></svg>';
   const h3Location = h3.cloneNode(true);
   divLocation.appendChild(h3Location);
-  h3Location.textContent =
-    "1234 Unknown Street, Anywhere City, Philippines 0000";
+  h3Location.textContent = "1234 Unknown Street, Anywhere City";
 
   const divFeedback = createDiv.cloneNode(true);
   divFeedback.classList.add("feedback");
@@ -94,47 +93,67 @@ export function ContactUs() {
   form.setAttribute("method", "POST");
   divForm.appendChild(form);
 
+  const nameDiv = createDiv.cloneNode(true);
+  nameDiv.classList.add("form-divs");
+  form.appendChild(nameDiv);
   const nameLabel = label.cloneNode(true);
   nameLabel.setAttribute("for", "feedback-name");
   nameLabel.textContent = "Name:";
-  form.appendChild(nameLabel);
+  nameDiv.appendChild(nameLabel);
   const nameInput = input.cloneNode(true);
   nameInput.setAttribute("type", "text");
   nameInput.setAttribute("name", "feedback-name");
   nameInput.setAttribute("placeholder", "Enter your name");
   nameInput.id = "feedback-name";
-  nameLabel.appendChild(nameInput);
+  nameDiv.appendChild(nameInput);
 
+  const emailDiv = createDiv.cloneNode(true);
+  emailDiv.classList.add("form-divs");
+  form.appendChild(emailDiv);
   const emailLabel = label.cloneNode(true);
   emailLabel.setAttribute("for", "feedback-email");
   emailLabel.textContent = "Email:";
-  form.appendChild(emailLabel);
+  emailDiv.appendChild(emailLabel);
   const emailInput = input.cloneNode(true);
   emailInput.setAttribute("type", "text");
   emailInput.setAttribute("name", "feedback-email");
   emailInput.setAttribute("placeholder", "Enter your email");
   emailInput.id = "feedback-email";
-  emailLabel.appendChild(emailInput);
+  emailDiv.appendChild(emailInput);
 
+  const subjectDiv = createDiv.cloneNode(true);
+  subjectDiv.classList.add("form-divs");
+  form.appendChild(subjectDiv);
   const subjectLabel = label.cloneNode(true);
   subjectLabel.setAttribute("for", "feedback-subject");
   subjectLabel.textContent = "Subject:";
-  form.appendChild(subjectLabel);
+  subjectDiv.appendChild(subjectLabel);
   const subjectInput = input.cloneNode(true);
   subjectInput.setAttribute("type", "text");
   subjectInput.setAttribute("name", "feedback-subject");
   subjectInput.setAttribute("placeholder", "Enter subject here");
   subjectInput.id = "feedback-subject";
-  subjectLabel.appendChild(subjectInput);
+  subjectDiv.appendChild(subjectInput);
 
+  const messageDiv = createDiv.cloneNode(true);
+  messageDiv.classList.add("form-divs");
+  form.appendChild(messageDiv);
   const messageLabel = label.cloneNode(true);
   messageLabel.setAttribute("for", "feedback-message");
   messageLabel.textContent = "Message:";
-  form.appendChild(messageLabel);
+  messageDiv.appendChild(messageLabel);
   const messageInput = document.createElement("textarea");
   messageInput.setAttribute("type", "text-area");
   messageInput.setAttribute("name", "feedback-message");
   messageInput.setAttribute("placeholder", "Enter your message here");
   messageInput.id = "feedback-message";
-  messageLabel.appendChild(messageInput);
+  messageDiv.appendChild(messageInput);
+
+  const submitDiv = createDiv.cloneNode(true);
+  submitDiv.classList.add("form-submit");
+  divFeedback.appendChild(submitDiv);
+  const submitInput = input.cloneNode(true);
+  submitInput.setAttribute("type", "submit");
+  submitInput.setAttribute("value", "Submit");
+  submitDiv.appendChild(submitInput);
 }
